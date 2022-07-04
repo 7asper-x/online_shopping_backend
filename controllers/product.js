4a2e331291c875a5e3d8ebc258e0d401c19fc8d4
jasper <lambokxk@gmail.com>
2022-07-03 22:57:02 -0700

    GET Endpoint

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product");
 const slugify = require("slugify");
+// const Category = require("../models/category");
 
 exports.create = async (req, res) => {
     try {
@@ -7,6 +8,15 @@ exports.create = async (req, res) => {
         req.body.slug = slugify(req.body.title);
         const newProduct = await new Product(req.body).save();
         res.json(newProduct);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+};
+
+exports.read = async (req, res) => {
+    try {
+        let product = await Product.find({});
+        res.json(product);
     } catch (err) {
         res.status(400).send(err.message);
     }
